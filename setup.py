1189d11a824e865eb0bef73fa3c2f0acfec1ef4a
Justin Ehlert <ehlertjd@gmail.com>
2018-04-27 17:40:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 
 NAME = "flywheel-bids"
-VERSION = "0.5.2"
+VERSION = "0.5.3"
 # To install the library, run the following
 #
 # python setup.py install
