b0d2b194a2017ba1f60b35aa609ca35e5436d958
Harsha Kethineni <harshakethineni@flywheel.io>
2018-09-10 15:34:19 -0500

    Allow  and  along with other conditions

--- flywheel_bids/supporting_files/templates.py
+++ flywheel_bids/supporting_files/templates.py
@@ -323,26 +323,15 @@ def test_where_clause(conditions, context):
     Returns:
         bool: True if the rule matches the given context.
     """
-    # Handle $or clauses at top-level
-    if '$or' in conditions:
-        for field, match in conditions['$or']:
-            value = utils.dict_lookup(context, field)
-            if processValueMatch(value, match):
-                return True
-        return False
-
-    # Otherwise AND clauses
-    if '$and' in conditions:
-        conditions = conditions['$and']
 
     for field, match in conditions.items():
         value = utils.dict_lookup(context, field)
-        if not processValueMatch(value, match):
+        if not processValueMatch(value, match, field):
             return False
 
     return True
 
-def processValueMatch(value, match):
+def processValueMatch(value, match, condition=None):
     """
     Helper function that recursively performs value matching.
     Args:
@@ -351,6 +340,23 @@ def processValueMatch(value, match):
     Returns:
         bool: The result of matching the value against the match spec.
     """
+    if condition:
+        # Handle $or clauses at top-level
+        if '$or' in conditions:
+            for field, deeper_match in match:
+                value = utils.dict_lookup(context, field)
+                if processValueMatch(value, deeper_match):
+                    return True
+            return False
+
+        # Otherwise AND clauses
+        if '$and' in conditions:
+            for field, deeper_match in match:
+                value = utils.dict_lookup(context, field)
+                if not processValueMatch(value, deeper_match):
+                    return False
+            return True
+
     if isinstance(match, dict):
         # Deeper processing
         if '$in' in match:
